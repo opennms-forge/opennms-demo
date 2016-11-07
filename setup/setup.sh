b046c370f57d5ca84ea889385dbd0199523d7789
Ronny Trommer <ronny@opennms.org>
2016-11-07 16:41:49 +0100

    Add script to initiliaze nodes with provisioning

---
+++ setup/setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+OPENNMS_HOST=docker
+OPENNMS_USER=admin
+OPENNMS_PASS=admin
+
+curl -v -u ${OPENNMS_USER}:${OPENNMS_PASS} \
+     -X POST \
+     -H "Content-Type: application/xml" \
+     -H "Accept: application/xml" \
+     -d @foreign-sources.xml \
+     http://${OPENNMS_HOST}:8980/opennms/rest/foreignSources
+
+curl -v -u ${OPENNMS_USER}:${OPENNMS_PASS} \
+     -X POST \
+     -H "Content-Type: application/xml" \
+     -H "Accept: application/xml" \
+     -d @demo-environment.xml \
+     http://${OPENNMS_HOST}:8980/opennms/rest/requisitions
+
+curl -v -u ${OPENNMS_USER}:${OPENNMS_PASS} \
+     -X PUT \
+     http://${OPENNMS_HOST}:8980/opennms/rest/requisitions/demo-environment/import
